1cd8902a6bebe23de07d98ec29c67fc265e212b6
Zdenek Kasner <kasnezde@fit.cvut.cz>
2016-10-10 18:54:54 +0200

    Add list of top members sorted by points

--- app/presenters/StatsPresenter.php
+++ app/presenters/StatsPresenter.php
@@ -18,15 +18,35 @@ class StatsPresenter extends BaseSecuredPresenter
     * @var \App\Model\UsersModel */
     public $usersModel;
 
+    /** 
+    * @inject
+    * @var \App\Model\ImageStorage */
+    public $imageStorage;
+
     public function __construct(Nette\Database\Context $database)
     {
         parent::__construct();
         $this->database = $database;
     }
 
+    public function beforeRender() {
+        $this->template->addFilter('getimage', function ($id_member) {
+            $member = $this->database->table('members_member')->get($id_member);
+            return $this->imageStorage->getProfileImage($member);
+        });
+    }
+
     public function renderDefault()
     {
         $this->template->ranks = $this->usersModel->getRanks();
         $this->template->ranks_json = Nette\Utils\Json::encode($this->usersModel->getRanks());
+
+        $this->template->points = $this->database->query('SELECT members_member.id_member, members_member.name, members_member.surname, SUM( COALESCE( members_points.points, members_activities.points ) ) AS sum
+                                                            FROM members_points
+                                                            JOIN members_activities USING (id_activity)
+                                                            JOIN members_member USING (id_member)
+                                                            GROUP BY id_member
+                                                            ORDER BY sum DESC');
+
     }
 }
