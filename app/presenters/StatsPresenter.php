dfd58cd677813df2e1372ea7d56cce93fd26c45c
David Příhoda <prihoda1@fit.cvut.cz>
2016-10-20 19:08:08 +0200

    new member stats

--- app/presenters/StatsPresenter.php
+++ app/presenters/StatsPresenter.php
@@ -41,7 +41,7 @@ class StatsPresenter extends BaseSecuredPresenter
         $this->template->ranks = $this->usersModel->getRanks();
         $this->template->ranks_json = Nette\Utils\Json::encode($this->template->ranks);
 
-        $this->template->new_member_counts = $this->database->query('SELECT members_member.joined as name, COUNT(members_member.id_member) as y FROM members_member GROUP BY (name) ORDER BY name ');
+        $this->template->new_member_counts = $this->database->query('SELECT DATE_FORMAT(members_member.joined,"%Y/%m") as join_date, COUNT(members_member.id_member) as y FROM members_member GROUP BY (join_date) HAVING join_date IS NOT NULL ORDER BY join_date');
         $this->template->new_member_counts_json = Nette\Utils\Json::encode($this->template->new_member_counts);
 
         $this->template->points = $this->database->query('SELECT members_member.id_member, members_member.name, members_member.surname, SUM( COALESCE( members_points.points, members_activities.points ) ) AS sum
