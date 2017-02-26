6e445866ed37e2d4a15c298528ff5158d5ef1c21
Zdenek Kasner <kasnezde@fit.cvut.cz>
2017-02-26 13:06:08 +0100

    Change function which disappeared with a new version of jquery

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -1,16 +1,16 @@
 /* Volání AJAXu u všech odkazů s třídou ajax */
-$("a.ajax").live("click", function (event) {
+$("a.ajax").on("click", function (event) {
     event.preventDefault();
     $.get(this.href);
 });
 
 /* AJAXové odeslání formulářů */
-$("form.ajax").live("submit", function () {
+$("form.ajax").on("submit", function () {
     $(this).ajaxSubmit();
     return false;
 });
 
-$("form.ajax :submit").live("click", function () {
+$("form.ajax :submit").on("click", function () {
     $(this).ajaxSubmit();
     return false;
 });
