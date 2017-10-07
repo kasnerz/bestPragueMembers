a141a1cd247e0a637ad2922666e6745469811f54
hajekvojtech <vojta.hajek@storyous.com>
2017-10-07 10:16:16 +0200

    API layer for Zapier

--- app/presenters/ApiPresenter.php
+++ app/presenters/ApiPresenter.php
@@ -3,15 +3,34 @@
 namespace App\Presenters;
 
 use Nette;
-use App\Forms\SignFormFactory;
 use Tracy\Dumper;
 
 class ApiPresenter extends BasePresenter
 {
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function injectDatabase(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
 
     public function actionAddCarrots($data)
     {
-        $this->sendResponse(new Nette\Application\Responses\TextResponse(Dumper::dump($data)));
-    }
+        $this->database->table("members_points")->insert(
+            array(
+                "id_batch" => 3,
+                "id_member" => 109,
+                "points" => 999,
+                "description" => $data
+            )
+        );
 
+        return 200;
+    }
 }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,8 +2,7 @@
 
 namespace App\Presenters;
 
-use Nette,
-    App\Model;
+use Nette;
 
 /**
  * LBG list
