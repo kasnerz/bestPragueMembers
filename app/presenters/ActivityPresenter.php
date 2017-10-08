7a36a8f58762b351f1e33224468cdec7c0d55eb7
hajekvojtech <vojta.hajek@storyous.com>
2017-10-08 11:47:52 +0200

    Made it easier in code and looking better

--- app/presenters/ActivityPresenter.php
+++ app/presenters/ActivityPresenter.php
@@ -20,8 +20,7 @@ class ActivityPresenter extends BaseSecuredPresenter {
     private $db_members;
     private $activities;
 
-    public function __construct(Nette\Database\Context $database)
-    {
+    public function __construct(Nette\Database\Context $database) {
         parent::__construct();
 
         \Nella\Forms\DateTime\DateInput::register();
@@ -32,8 +31,7 @@ class ActivityPresenter extends BaseSecuredPresenter {
                     ->select('id_member, members_member.name, members_member.surname')
                     ->order('name ASC');
         
-        $db_activities = $this->database->table('members_activities')->select('id_activity, name, points, description')
-        ->order('name');
+        $db_activities = $this->database->table('members_activities')->select('id_activity, name, points, description')->order('name');
         
         $this->activities = array();
 
@@ -45,6 +43,12 @@ class ActivityPresenter extends BaseSecuredPresenter {
     public function renderNew() {
         $this->template->members = $this->database->table('members_member');
         $this->template->points = $this->database->table('members_points');
+    }
+
+    public function makeStringsNull($values) {
+        foreach ($values as $i => $value) {
+            if ($value === "") $values[$i] = NULL;
+        }
     }
 
     public function renderBatch() {
@@ -76,6 +80,9 @@ class ActivityPresenter extends BaseSecuredPresenter {
         $this->template->db_members = $this->db_members;
     }
 
+    /*
+     * The list of user's carrots per this month, last month and last half-year
+     */
     public function renderDefault() {
         $user = $this->getUser()->getId();
 
@@ -99,13 +106,15 @@ class ActivityPresenter extends BaseSecuredPresenter {
         $this->template->members = $this->database->table('members_member');
         $this->template->points = $this->database->table('members_points')->where('approved', false)->order('id_points DESC');
 
-
         $filterId = isset($_GET['id_member']) ? $_GET['id_member'] : null;
+
         if($filterId) {
             $this->template->members = $this->template->members->where("id_member", $filterId);
             $this->template->points = $this->template->points->where("id_member", $filterId);
         }
+
         $filterMonth = isset($_GET['month']) ? $_GET['month'] : null;
+
         if($filterMonth) {
             $this->template->points = $this->template->points->where("datetime LIKE ?", str_replace('/','-',$filterMonth).'%');
         }
@@ -118,32 +127,6 @@ class ActivityPresenter extends BaseSecuredPresenter {
             return $this->imageStorage->getProfileImage($member);
         });
     }
-
-    private function makeFormLookGood($form) {
-        // All of this is only additional code that makes Nette form look good in Bootstrap
-        // setup form rendering
-        $renderer = $form->getRenderer();
-        $renderer->wrappers['controls']['container'] = NULL;
-        $renderer->wrappers['pair']['container'] = 'div class=form-group';
-        $renderer->wrappers['pair']['.error'] = 'has-error';
-        $renderer->wrappers['control']['container'] = 'div class=col-sm-9';
-        $renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
-        $renderer->wrappers['control']['description'] = 'span class=help-block';
-        $renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
-        // make form and controls compatible with Twitter Bootstrap
-        $form->getElementPrototype()->class('form-horizontal');
-        foreach ($form->getControls() as $control) {
-            if ($control instanceof Controls\Button) {
-                $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-primary' : 'btn btn-default');
-                $usedPrimary = TRUE;
-            } elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
-                $control->getControlPrototype()->addClass('form-control');
-            } elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
-                $control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
-            }
-        }
-    }
-
 
     protected function createComponentPointsFormBatch()
     {
@@ -163,57 +146,13 @@ class ActivityPresenter extends BaseSecuredPresenter {
 
         $form->addSubmit('submit', 'Zapsat');
         $form->onSuccess[] = array($this, 'pointsFormBatchSucceeded');
-
-        $this->makeFormLookGood($form);
         
         return $form;
     }
 
-    protected function createComponentPointsForm()
-    {
-        $form = new Form;
-
-        $form->addSelect('id_activity', 'Aktivita*', $this->activities);
-        $form->addText('name', 'Název');
-        $form->addTextArea('description', 'Popis');
-
-        $form->addText('points', 'Počet mrkví')->setType('number');
-
-        $form->addDate('datetime', 'Datum*', 'Y-m-d')
-              ->setDefaultValue(\Nette\Utils\DateTime::createFromFormat('Y-m-d', date("Y-m-d")));
-
-        $form->addSubmit('submit', 'Přidat');
-        $form->onSuccess[] = array($this, 'pointsFormSucceeded');
-
-        $this->makeFormLookGood($form);
-
-        return $form;
-    }
-
-    public function makeStringsNull($values) {
-        foreach ($values as $i => $value) {
-            if ($value === "") $values[$i] = NULL;
-        }
-    }
-
-    public function pointsFormSucceeded($form, $values)
-    {   
-        $values[$this->user->getIdentity()->getId()] = TRUE;
-        $this->pointsFormBatchSucceeded($form, $values);
-    }
-
     public function pointsFormBatchSucceeded($form, $values)
-    {   
-        $user = $this->getUser();
+    {
         $this->makeStringsNull($values);
-
-        $last_row = $this->database->table('members_points')->order('id_batch DESC')->fetch();
-
-        if ($last_row == FALSE) {
-            $id_batch = 1;
-        } else {
-            $id_batch = $last_row->id_batch + 1;
-        }
 
         if(!$values['points'])
             $points = $this->database->table('members_activities')->where("id_activity", $values['id_activity'])->fetch();
@@ -224,16 +163,15 @@ class ActivityPresenter extends BaseSecuredPresenter {
             if (is_numeric($i) && $value != NULL) {
                 $this->database->table('members_points')->insert(array(
                     'id_member' => $i,
-                    'id_batch' => $id_batch,
                     'id_activity' => $values['id_activity'],
                     'points' => $points,
                     'name' => $values['name'],
                     'description' => $values['description'],
                     'datetime' => $values['datetime'],
+					'id_author' => $this->getUser()->getId(),
 					'approved' => 0
                     ));
             }
-
         }
 
         $this->flashMessage("Díky za návrh na mrkvičky! HRoyt na to koukne :-)", 'success');
@@ -244,14 +182,11 @@ class ActivityPresenter extends BaseSecuredPresenter {
         $this->database->table('members_points')->get($id)->delete();
         $this->flashMessage('Aktivita smazána.');
         $this->redirect('Activity:approvals');
-        // $this->redirect('Homepage:');
     }
 
     public function handleApprove($id) {
         $this->database->table('members_points')->get($id)->update(array('approved' => 1));
         $this->flashMessage('Aktivita schválena.');
         $this->redirect('Activity:approvals');
-        // $this->redirect('Homepage:');
     }
-	
 }
